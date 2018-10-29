c9c8b09c29943434520f9d82ad8c963bcb053d62
Hysun He <hysunhe@foxmail.com>
2018-10-29 18:57:41 +0800

    dev

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import * as echarts from 'echarts';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map'
 
 @Component({
   selector: 'page-contact',
@@ -7,8 +10,76 @@ import { NavController } from 'ionic-angular';
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public http: Http) {
+    console.log("*** LuGuTong page ***");
   }
 
+  ionViewDidEnter() {
+    this.loadLuGuTongPoints();
+  }
+
+  loadLuGuTongPoints() {
+    this.http.get("http://localhost:8080/sync-service/data/lugutongchart?limit=100")
+      .map(res => res.json()).subscribe(data => {
+        let dataArray = [], labelArray = [];
+        data.forEach(e => {
+          dataArray.unshift(e.value);
+          labelArray.unshift(e.date);
+        });
+        const ec = echarts as any;
+        var dapanchart = ec.init(document.getElementById('lugutongchart'));
+        var option = {
+          tooltip: {
+            trigger: 'axis',
+            position: function (pt) {
+              return [pt[0], '10%'];
+            }
+          },
+          title: {
+            left: 'center',
+            text: '陆股通汇总数据图',
+          },
+          toolbox: {
+            feature: {
+              dataZoom: {
+                yAxisIndex: 'none'
+              },
+              restore: {},
+              saveAsImage: {}
+            }
+          },
+          xAxis: {
+            type: 'category',
+            boundaryGap: false,
+            data: labelArray
+          },
+          yAxis: {
+            type: 'value',
+            boundaryGap: [0, '100%']
+          },
+          series: [
+            {
+              name: '资金变化',
+              type: 'line',
+              itemStyle: {
+                color: 'rgb(255, 70, 131)'
+              },
+              areaStyle: {
+                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                  offset: 0,
+                  color: 'red'
+                }, {
+                  offset: 1,
+                  color: 'green'
+                }])
+              },
+              data: dataArray
+            }
+          ]
+        };
+        dapanchart.setOption(option);
+      }, (err) => {
+        console.error("!!! Failed loading dapan points!");
+      });
+  }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import * as echarts from 'echarts';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map'
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,78 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public http: Http) {
+    console.log("*** Home page ***");
+    console.log("* Echarts info: ", echarts);
+    console.log("*** Goodbye ***");
   }
 
+  ionViewDidEnter() {
+    this.loadDapanPoints();
+  }
+
+  loadDapanPoints() {
+    this.http.get("http://localhost:8080/sync-service/data/dapanchart?limit=100")
+      .map(res => res.json()).subscribe(data => {
+        let dataArray = [], labelArray = [];
+        data.forEach(e => {
+          dataArray.unshift(e.value);
+          labelArray.unshift(e.date);
+        });
+        const ec = echarts as any;
+        var dapanchart = ec.init(document.getElementById('dapanchart'));
+        var option = {
+          tooltip: {
+            trigger: 'axis',
+            position: function (pt) {
+              return [pt[0], '10%'];
+            }
+          },
+          title: {
+            left: 'center',
+            text: '大资金进出数据图',
+          },
+          toolbox: {
+            feature: {
+              dataZoom: {
+                yAxisIndex: 'none'
+              },
+              restore: {},
+              saveAsImage: {}
+            }
+          },
+          xAxis: {
+            type: 'category',
+            boundaryGap: false,
+            data: labelArray
+          },
+          yAxis: {
+            type: 'value',
+            boundaryGap: [0, '100%']
+          },
+          series: [
+            {
+              name: '资金变化',
+              type: 'line',
+              itemStyle: {
+                color: 'rgb(255, 70, 131)'
+              },
+              areaStyle: {
+                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                  offset: 0,
+                  color: 'red'
+                }, {
+                  offset: 1,
+                  color: 'green'
+                }])
+              },
+              data: dataArray
+            }
+          ]
+        };
+        dapanchart.setOption(option);
+      }, (err) => {
+        console.error("!!! Failed loading dapan points!");
+      });
+  }
 }
