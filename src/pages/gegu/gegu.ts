55b798903e840ebccba4fc5fcb0856441e0bc44b
Hysun He <hysunhe@foxmail.com>
2018-10-30 17:29:48 +0800

    dev

--- src/pages/gegu/gegu.ts
+++ src/pages/gegu/gegu.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-about',
-  templateUrl: 'about.html'
+  selector: 'page-gegu',
+  templateUrl: 'gegu.html'
 })
-export class AboutPage {
+export class GeguPage {
 
   constructor(public navCtrl: NavController) {
 
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
+import { GeguPage } from '../gegu/gegu';
 import { ContactPage } from '../contact/contact';
 import { HomePage } from '../home/home';
 
@@ -11,7 +11,7 @@ export class TabsPage {
 
   tab1Root = HomePage;
   tab2Root = ContactPage;
-  tab3Root = AboutPage;
+  tab3Root = GeguPage;
 
   constructor() {
 
