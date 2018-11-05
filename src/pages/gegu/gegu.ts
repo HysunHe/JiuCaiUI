6d3dccabf13415130fc45634cffee7bde5153cec
Hysun He <hysunhe@foxmail.com>
2018-11-05 17:43:57 +0800

    dev

--- src/pages/gegu/gegu.ts
+++ src/pages/gegu/gegu.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import * as echarts from 'echarts';
 import { Storage } from '@ionic/storage';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
@@ -9,29 +10,84 @@ import 'rxjs/add/operator/map';
   templateUrl: 'gegu.html'
 })
 export class GeguPage {
-  historyStocks: string[];
+  items: any;
+  matchedItems: any;
+  selectedItem: any;
+  histItems: any;
+  showList: boolean;
+  showHistCharts: any = {};
 
-  constructor(private storage: Storage, private http: Http, private navCtrl: NavController) {
-    this.historyStocks = [];
+  constructor(private storage: Storage, private http: Http, navCtrl: NavController) {
+    this.showHistCharts.hist = true;
+    this.showHistCharts.geguchart = false;
+    this.matchedItems = [];
+    this.selectedItem = {code: '', name: '', pinYin: '', firstLetters: ''};
+    this.items = storage.get("stocks");
+    this.histItems = storage.get("histItems").then((val) => {
+      if(val === null) {
+        this.histItems = [];
+      } else {
+        this.histItems = val;
+      }
+      console.log("histItems", this.histItems);
+      for(let i=1; i<=this.histItems.length; i++) {
+        console.log(this.histItems[i-1]);
+        this.loadLuGuTongPoints(this.histItems[i-1], 'geguchart' + i);
+        this.showHistCharts["geguchart" + i] = true;
+      }
+    });
+
+    http.get('http://localhost:8080/sync-service/data/listStocks')
+    .map(res => res.json())
+    .subscribe(data => {
+       this.items = data; 
+       storage.set("stocks", data);
+    });
   }
 
-  getHistoryStocks(ev: any) {
+  getMatchedStocks(ev: any) {
+    this.showList = true;
     // set val to the value of the searchbar
     const val = ev.target.value;
-
+    console.log("*** val: " + ev.target.value);
     // if the value is an empty string don't filter the items
     if (val && val.trim() != '') {
-      this.historyStocks = this.historyStocks.filter((item) => {
-        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      this.matchedItems = this.items.filter((item) => {
+        return (item.code.indexOf(val) > -1);
       })
     }
   }
 
+<<<<<<< HEAD
   loadLuGuTongPoints(ev, item) {
     console.log(ev);
     console.log(item);
     var code = "600360";
     
+=======
+  itemTapped(ev:any, item:any) { 
+    this.showList = false;
+    this.showHistCharts.hist = false;
+    this.showHistCharts.geguchart = true;
+    this.selectedItem.name = item.name;
+    this.selectedItem.code = item.code;
+    this.selectedItem.firstLetters = item.firstLetters;
+    this.selectedItem.pinYin = item.pinYin;
+    this.loadLuGuTongPoints(item, 'geguchart');
+
+    let tempHistItems = this.histItems.filter((e) => e.code !== item.code);
+    tempHistItems.unshift(item);
+    if(tempHistItems.length > 20) {
+      tempHistItems.pop();
+    }
+    this.histItems = tempHistItems;
+    this.storage.set("histItems", this.histItems);
+  }
+
+  loadLuGuTongPoints(item: any, id: string) {
+    let code: string = item.code;
+    console.log("*** Generating chart: " + id + " | code: " + code);
+>>>>>>> dev
     this.http.get("http://localhost:8080/sync-service/data/geguchart?code=" + code + "&limit=30")
       .map(res => res.json()).subscribe(data => {
         let dataArray = [], labelArray = [];
@@ -40,7 +96,7 @@ export class GeguPage {
           labelArray.unshift(e.date);
         });
         const ec = echarts as any;
-        var dapanchart = ec.init(document.getElementById('geguchart'));
+        var dapanchart = ec.init(document.getElementById(id));
         var option = {
           tooltip: {
             trigger: 'axis',
@@ -50,7 +106,7 @@ export class GeguPage {
           },
           title: {
             left: 'center',
-            text: '个股日汇总统计图',
+            text: item.name + '日汇总',
           },
           toolbox: {
             feature: {
